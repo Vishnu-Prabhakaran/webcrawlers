2ded5322e346396ac72c5d73a9ca4099bdc0f6ef
Vishnu-Prabhakaran <vampire.wish@gmail.com>
2020-02-23 01:44:35 +1100

    Crawler - Checking robot.txt rules

--- robotparser.js
+++ robotparser.js
@@ -1,22 +1,40 @@
-var robotsParser = require('robots-parser');
+var robotParser = require('robots-parser');
+const request = require('request-promise');
 
-var robots = robotsParser(
-  'http://example.com/robots.txt',
-  [
-    'User-agent: *',
-    'Disallow: /dir/',
-    'Disallow: /test.html',
-    'Allow: /dir/test.html',
-    'Allow: /test.html',
-    'Crawl-delay: 1',
-    'Sitemap: http://example.com/sitemap.xml',
-    'Host: example.com'
-  ].join('\n')
-);
+const robotsUrl = 'https://textfiles.meulie.net/robots.txt';
 
-console.log(robots.isAllowed('http://example.com/test.html', 'Sams-bot/1.0')); //false
-robots.isAllowed('http://example.com/dir/test.html', 'Sams-bot/1.0'); // true
-robots.isDisallowed('http://example.com/dir/test2.html', 'Sams-Bot/1.0'); // true
-robots.getCrawlDelay('Sams-bot/1.0'); // 1 sec
-robots.getSitemaps(); // http://example.com/sitemap.xml
-//robots.getPreferredHost(); //example.com
+async function getRbotsTxt(robotsUrl) {
+  const robotTxt = await request.get(robotsUrl);
+  const robots = robotParser(robotsUrl, robotTxt);
+  //return robotTxt;
+  console.log(
+    robots.isAllowed('https://textfiles.meulie.net/100/', 'YourNameBot') // do not exist - no rule
+  );
+  console.log(
+    robots.isAllowed('https://textfiles.meulie.net/100/', 'rogerbot') // exist - no crawling
+  );
+  console.log(robots.getCrawlDelay());
+}
+
+getRbotsTxt(robotsUrl);
+
+// var robots = robotsParser(
+//   'http://example.com/robots.txt',
+//   [
+//     'User-agent: *',
+//     'Disallow: /dir/',
+//     'Disallow: /test.html',
+//     'Allow: /dir/test.html',
+//     'Allow: /test.html',
+//     'Crawl-delay: 1',
+//     'Sitemap: http://example.com/sitemap.xml',
+//     'Host: example.com'
+//   ].join('\n')
+// );
+
+// console.log(robots.isAllowed('http://example.com/test.html', 'Sams-bot/1.0')); //false
+// robots.isAllowed('http://example.com/dir/test.html', 'Sams-bot/1.0'); // true
+// robots.isDisallowed('http://example.com/dir/test2.html', 'Sams-Bot/1.0'); // true
+// robots.getCrawlDelay('Sams-bot/1.0'); // 1 sec
+// robots.getSitemaps(); // http://example.com/sitemap.xml
+// //robots.getPreferredHost(); //example.com
